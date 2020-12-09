be7ceafa836c570f6a722a072e260270054ae663
danagelh <66285394+danagelh@users.noreply.github.com>
2020-12-08 20:49:22 -0500

    Update pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -23,6 +23,7 @@ class Pokedex(object):
                 print("Pokedex Encyclopedia")
                 print("1: Search by ID")
                 print("2: Search by name")
+                print("3: Search by type")
                 print("0: Exit")
                 while True:
                     user_input = input("Enter: ")
@@ -36,13 +37,14 @@ class Pokedex(object):
                         # Search by ID
                         elif user_input == 1:
                             id_input = input("Enter ID to search for: ")
-                            try:
-                                id_input = int(id_input)
-                                print(poke_list[id_input - 1])
-                            except ValueError:
-                                print("ID not an integer")
-                            except IndexError:
-                                print("Pokedex only contains IDs up to", len(poke_list))
+                            if int(id_input) <= 0 or int(id_input) > poke_list.size:
+                                print("ID not found\n")
+                            else:
+                                try:
+                                    id_input = int(id_input)
+                                    print(poke_list[id_input - 1])
+                                except ValueError:
+                                    print("ID not an integer\n")
 
                         # Search by name
                         elif user_input == 2:
@@ -53,24 +55,39 @@ class Pokedex(object):
                                     print(members)
                                     name_found = True
                             if name_found == False:
-                                print("Name not found")
+                                print("Name not found\n")
+
+                        # Search by type
+                        elif user_input == 3:
+                            type_input = input("Enter type to search for: ")
+                            type_found = False
+                            for members in poke_list:
+                                for type in members.types:
+                                    if type == type_input.lower():
+                                        print(members)
+                                        type_found = True
+                            if type_found == False:
+                                    print("Type not found\n")
+
 
                         # Invalid input
                         else:
-                            print("Please choose a valid number")
+                            print("Please choose a valid number\n")
 
                         print("1: Search by ID")
                         print("2: Search by name")
+                        print("3: Search by type")
                         print("0: Exit")
 
                     except ValueError:
-                        print("Please enter a number \n")
+                        print("Please enter a number\n")
                         print("1: Search by ID")
                         print("2: Search by name")
+                        print("3: Search by type")
                         print("0: Exit")
 
             except IOError:
-                print("Error: pokedex.json file does not exist.")
+                print("Error: pokedex.json file does not exist.\n")
                 return
 
 
